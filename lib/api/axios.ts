1aa4af51954e0daf20f48ba26f60a4e29fcd48e7
batsal <shresthabatsal247@gmail.com>
2026-02-02 00:24:39 +0545

    update axios instance with token interceptor

--- lib/api/axios.ts
+++ lib/api/axios.ts
@@ -1,11 +1,28 @@
 import axios from "axios";
+import { getAuthToken } from "@/lib/cookie";
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5050';
+const BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5050";
+
 const axiosInstance = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    }
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+// Attach token
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const token = await getAuthToken();
+    if (token && config.headers) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
